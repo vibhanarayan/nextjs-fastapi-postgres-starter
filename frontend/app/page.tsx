f80f3a2cec25e6dfa4eece46f3238e5e8c7e0d22
Vibha Narayan <vibhanarayan@ip-192-168-0-6.ec2.internal>
2024-07-13 15:45:32 -0500

    change double quotes to single quotes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,18 @@
-import Image from "next/image";
-import Chat from "./components/Chat";
+import Image from 'next/image';
+import Chat from './components/Chat';
 import { User } from './types';
 
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export default async function Home() {
-  console.log("fetch", `${apiUrl}/users/me`);
+  console.log('fetch', `${apiUrl}/users/me`);
   const user: User = await fetch(`${apiUrl}/users/me`).then((res) =>
     res.json()
   );
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       Hello, {user.name}!
       <Chat user={user} />
     </main>
